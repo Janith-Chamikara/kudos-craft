9938fadaf74c2bd66235d31f28efeb9f508b0d37
Janith-Chamikara <janithchamikara13@gmail.com>
2025-11-09 14:27:46 +0530

    test

--- server.tf
+++ server.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"   
 }
+//trigger test
 
- 
 resource "aws_instance" "example" {
   ami           = "ami-0157af9aea2eef346"  
   instance_type = "t3.micro"                
