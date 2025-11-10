215fbf0de4bc3886239509dea076f6dbcb835328
Janith-Chamikara <janithchamikara13@gmail.com>
2025-11-10 14:06:00 +0530

    Update: ami id

--- server.tf
+++ server.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "vm_janith" {
-  ami           = "ami-07fb0a5bf9ae299a4"  
+  ami           = "ami-000bc3d0f4b2707c9"  
   instance_type = "t3.micro"                
 
   tags = {
